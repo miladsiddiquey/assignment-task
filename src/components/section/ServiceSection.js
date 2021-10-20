778c8aee96129037823f11a2b0b0b24185a70287
marufasiddika4 <marufasiddika4@gmail.com>
2021-10-20 22:29:21 +0600

    a

--- src/components/section/ServiceSection.js
+++ src/components/section/ServiceSection.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Style from '../../style/ServiceSection.module.css';
 import ServiceData from '../../AllData/ServiceData.json';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
@@ -13,11 +14,11 @@ const ServiceSection = () => {
     return (
         <section>
            <div className="container">
-               <div className="row row-cols-md-2 row-cols-sm-2">
+               <div data-aos="fade-up"  data-aos-duration="2000" className="row  row-cols-md-2 row-cols-sm-2">
                    {
                        product.map((pro, index)=>{
                            return(
-                               <div key={index}  className="col-6">
+                               <div key={index}  className={`${Style.serviceCard} col-6`}>
                                   <div className="text-center">
                                   <img  src={pro.img} alt="" className="img-fluid" />
                                   <h4>{pro.title}</h4>
